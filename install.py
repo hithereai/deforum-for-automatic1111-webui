210fb019b46122659f899465c47e1efa3df65c4b
hithereai <121192995+hithereai@users.noreply.github.com>
2023-02-22 22:29:15 +0200

    fix rich for colab users again

--- install.py
+++ install.py
@@ -8,7 +8,7 @@ with open(req_file) as file:
     for lib in file:
         lib = lib.strip()
         if not launch.is_installed(lib):
-            if lib == 'rich':
-                launch.run(f'"{sys.executable}" -m pip install {lib}', desc=f"Installing Deforum requirement: {lib}", errdesc=f"Couldn't install {lib}")
+            if lib == 'rich' and 'google.colab' in sys.modules:
+                launch.run(f'"{sys.executable}" -m pip install {lib} --no-dependencies', desc=f"Installing Deforum requirement: {lib}", errdesc=f"Couldn't install {lib}")
             else:
                 launch.run_pip(f"install {lib}", f"Deforum requirement: {lib}")
