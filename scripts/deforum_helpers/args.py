01185c783d50b187f9972ccdccb05e4a968343ce
hithereai <121192995+hithereai@users.noreply.github.com>
2023-03-31 11:06:45 +0300

    more

--- scripts/deforum_helpers/args.py
+++ scripts/deforum_helpers/args.py
@@ -401,12 +401,12 @@ def setup_deforum_setting_dictionary(self, is_img2img, is_extension = True):
             with gr.TabItem('Keyframes'): #TODO make a some sort of the original dictionary parsing
                 with gr.Row(variant='compact'):
                     with gr.Column(scale=2):
-                        animation_mode = gr.Radio(['2D', '3D', 'Interpolation', 'Video Input'], label="Animation mode", value=da.animation_mode, elem_id="animation_mode")
+                        animation_mode = gr.Radio(['2D', '3D', 'Interpolation', 'Video Input'], label="Animation mode", value=da.animation_mode, elem_id="animation_mode", info="control animation mode, will hide non relevant params upon change")
                     with gr.Column(scale=1, min_width=180):
-                        border = gr.Radio(['replicate', 'wrap'], label="Border", value=da.border, elem_id="border")
+                        border = gr.Radio(['replicate', 'wrap'], label="Border", value=da.border, elem_id="border", info="controls pixel generation method for images smaller than the frame. hover on the options to see more info")
                 with gr.Row(variant='compact'):
-                    diffusion_cadence = gr.Slider(label="Cadence", minimum=1, maximum=50, step=1, value=da.diffusion_cadence, interactive=True)
-                    max_frames = gr.Number(label="Max frames", lines=1, value = da.max_frames, interactive=True, precision=0)
+                    diffusion_cadence = gr.Slider(label="Cadence", minimum=1, maximum=50, step=1, value=da.diffusion_cadence, interactive=True, info="# of in-between frames that will not be directly diffused")
+                    max_frames = gr.Number(label="Max frames", lines=1, value = da.max_frames, interactive=True, precision=0, info="end the animation at this frame number")
                 # GUIDED IMAGES ACCORD
                 with gr.Accordion('Guided Images', open=False, elem_id='guided_images_accord') as guided_images_accord:
                     # GUIDED IMAGES INFO ACCORD
