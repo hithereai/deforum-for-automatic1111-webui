c6c18a948da3241296057ccce524026da8009c06
hithereai <121192995+hithereai@users.noreply.github.com>
2023-04-26 14:40:11 +0300

    p

--- scripts/deforum_helpers/deforum_controlnet.py
+++ scripts/deforum_helpers/deforum_controlnet.py
@@ -87,7 +87,7 @@ def setup_controlnet_ui_raw():
             control_mode = gr.Radio(choices=["Balanced", "My prompt is more important", "ControlNet is more important"], value="Balanced", label="Control Mode", interactive=True)            
         with gr.Row(visible=False) as env_row:
             resize_mode = gr.Radio(choices=["Outer Fit (Shrink to Fit)", "Inner Fit (Scale to Fit)", "Just Resize"], value="Inner Fit (Scale to Fit)", label="Resize Mode", interactive=True)
-        hide_output_list = [pixel_perfect,low_vram,mod_row,module,weight_row,env_row,overwrite_frames,vid_path_row,mask_vid_path_row,advanced_column,control_mode_row] 
+        hide_output_list = [pixel_perfect,low_vram,mod_row,module,weight_row,env_row,overwrite_frames,vid_path_row,mask_vid_path_row,control_mode_row] 
         for cn_output in hide_output_list:
             enabled.change(fn=hide_ui_by_cn_status, inputs=enabled,outputs=cn_output)
         module.change(build_sliders, inputs=[module, pixel_perfect], outputs=[processor_res, threshold_a, threshold_b, advanced_column])
