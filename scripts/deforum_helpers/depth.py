34c5cca4ec1ff6ef2eb0c506d6d6fa68e6850798
hithereai <121192995+hithereai@users.noreply.github.com>
2023-05-01 14:48:13 +0300

    no more use_zoe_depthh, now we have depth_algorithm

--- scripts/deforum_helpers/depth.py
+++ scripts/deforum_helpers/depth.py
@@ -18,44 +18,44 @@ class DepthModel:
     _instance = None
 
     def __new__(cls, *args, **kwargs):
-        keep_in_vram = kwargs.get('keep_in_vram', False)
-        use_zoe_depth = kwargs.get('use_zoe_depth', False)
-        Width = kwargs.get('Width', 512)
-        Height = kwargs.get('Height', 512)
-        model_switched = cls._instance and cls._instance.use_zoe_depth != use_zoe_depth
-        resolution_changed = cls._instance and (cls._instance.Width != Width or cls._instance.Height != Height)
+            keep_in_vram = kwargs.get('keep_in_vram', False)
+            depth_algorithm = kwargs.get('depth_algorithm', 'Midas')
+            Width = kwargs.get('Width', 512)
+            Height = kwargs.get('Height', 512)
+            model_switched = cls._instance and cls._instance.depth_algorithm != depth_algorithm
+            resolution_changed = cls._instance and (cls._instance.Width != Width or cls._instance.Height != Height)
 
-        if cls._instance is None or (not keep_in_vram and not hasattr(cls._instance, 'midas_model')) or model_switched or resolution_changed:
-            cls._instance = super().__new__(cls)
-            cls._instance._initialize(models_path=args[0], device=args[1], half_precision=True, keep_in_vram=keep_in_vram, use_zoe_depth=use_zoe_depth, Width=Width, Height=Height)
-        elif cls._instance.should_delete and keep_in_vram:
-            cls._instance._initialize(models_path=args[0], device=args[1], half_precision=True, keep_in_vram=keep_in_vram, use_zoe_depth=use_zoe_depth, Width=Width, Height=Height)
-        cls._instance.should_delete = not keep_in_vram
-        return cls._instance
+            if cls._instance is None or (not keep_in_vram and not hasattr(cls._instance, 'midas_model')) or model_switched or resolution_changed:
+                cls._instance = super().__new__(cls)
+                cls._instance._initialize(models_path=args[0], device=args[1], half_precision=True, keep_in_vram=keep_in_vram, depth_algorithm=depth_algorithm, Width=Width, Height=Height)
+            elif cls._instance.should_delete and keep_in_vram:
+                cls._instance._initialize(models_path=args[0], device=args[1], half_precision=True, keep_in_vram=keep_in_vram, depth_algorithm=depth_algorithm, Width=Width, Height=Height)
+            cls._instance.should_delete = not keep_in_vram
+            return cls._instance
 
-    def _initialize(self, models_path, device, half_precision=True, keep_in_vram=False, use_zoe_depth=False, Width=512, Height=512):
-            self.keep_in_vram = keep_in_vram
-            self.Width = Width
-            self.Height = Height
-            self.adabins_helper = None
-            self.depth_min = 1000
-            self.depth_max = -1000
-            self.device = device
-            self.use_zoe_depth = use_zoe_depth
+    def _initialize(self, models_path, device, half_precision=True, keep_in_vram=False, depth_algorithm='midas', Width=512, Height=512):
+        self.keep_in_vram = keep_in_vram
+        self.Width = Width
+        self.Height = Height
+        self.adabins_helper = None
+        self.depth_min = 1000
+        self.depth_max = -1000
+        self.device = device
+        self.depth_algorithm = depth_algorithm
 
-            if self.use_zoe_depth:
-                self.zoe_depth = ZoeDepth(self.Width, self.Height)
-            else:
-                self.midas_depth = MidasDepth(models_path, device, half_precision=half_precision)
-
+        if self.depth_algorithm == 'Zoe':
+            self.zoe_depth = ZoeDepth(self.Width, self.Height)
+        else:
+            self.midas_depth = MidasDepth(models_path, device, half_precision=half_precision)
+            
     def predict(self, prev_img_cv2, midas_weight, half_precision) -> torch.Tensor:
         use_adabins = midas_weight < 1.0 and self.adabins_helper is not None
 
         img_pil = Image.fromarray(cv2.cvtColor(prev_img_cv2.astype(np.uint8), cv2.COLOR_RGB2BGR))
 
-        if self.use_zoe_depth:
+        if self.depth_algorithm == 'Zoe':
             depth_tensor = self.zoe_depth.predict(img_pil).to(self.device)
-        else:
+        else: # only other working implementation rn is MidasAdaBins (combo of the two)
             depth_tensor = self.midas_depth.predict_depth(prev_img_cv2, half_precision)
 
         self.debug_print("Shape of depth_tensor:", depth_tensor.shape)
@@ -64,12 +64,12 @@ class DepthModel:
         if use_adabins: # need to use AdaBins. first, try to get adabins depth estimation from our image
             use_adabins, adabins_depth = AdaBinsModel._instance.predict(img_pil, prev_img_cv2, use_adabins)
             if use_adabins: # if there was no error in getting the depth, align other depths (midas/zoe/leres) with adabins' depth
-                depth_tensor = self.blend_and_align_with_adabins(depth_tensor, adabins_depth, midas_weight, self.use_zoe_depth)
+                depth_tensor = self.blend_and_align_with_adabins(depth_tensor, adabins_depth, midas_weight)
 
         return depth_tensor
 
-    def blend_and_align_with_adabins(self, depth_tensor, adabins_depth, midas_weight, use_zoe_depth):
-        depth_tensor = torch.subtract(50.0, depth_tensor) / 19.0
+    def blend_and_align_with_adabins(self, depth_tensor, adabins_depth, midas_weight):
+        depth_tensor = torch.subtract(50.0, depth_tensor) / 19.0 # this alignment was originally meant for midas, but it kinda works with Zoe and Leres too
         blended_depth_map = (depth_tensor.cpu().numpy() * midas_weight + adabins_depth * (1.0 - midas_weight))
         depth_tensor = torch.from_numpy(np.expand_dims(blended_depth_map, axis=0)).squeeze().to(self.device)
         return depth_tensor
@@ -89,7 +89,7 @@ class DepthModel:
 
     def to(self, device):
         self.device = device
-        if self.use_zoe_depth:
+        if self.depth_algorithm == 'Zoe':
             self.zoe_depth.zoe.to(device)
         else:
             self.midas_model.to(device)
@@ -99,7 +99,7 @@ class DepthModel:
         torch.cuda.empty_cache()
 
     def delete_model(self):
-        if self.use_zoe_depth:
+        if self.depth_algorithm == 'Zoe':
             self.zoe_depth.delete()
             del self.zoe_depth
         else:
